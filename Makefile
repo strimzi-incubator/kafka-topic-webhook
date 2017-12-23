cb73fcdc987aedd75a2951a2d01ddb7570d7e268
Jakub Scholz <www@scholzj.com>
2017-12-24 00:01:02 +0100

    Add SSL support for webserver

--- Makefile
+++ Makefile
@@ -3,6 +3,13 @@ PROJECT_NAME=kafka-topic-initializer
 all: java_build docker_build docker_push
 clean: java_clean
 
+gen_certs:
+	echo "Generating certificates ..."
+	cfssl genkey -initca tls/ca.json | cfssljson -bare tls/ca
+	cfssl gencert -ca tls/ca.pem -ca-key tls/ca-key.pem tls/initializer.json | cfssljson -bare tls/initializer
+	mv tls/initializer.pem src/main/resources/initializer.pem
+	mv tls/initializer-key.pem src/main/resources/initializer-key.pem
+
 include ./Makefile.docker
 
 include ./Makefile.java
--- src/main/java/cz/scholz/kafka/topicinitializer/TopicWebhook.java
+++ src/main/java/cz/scholz/kafka/topicinitializer/TopicWebhook.java
@@ -5,10 +5,16 @@ import io.vertx.core.AbstractVerticle;
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Future;
 import io.vertx.core.Handler;
+import io.vertx.core.buffer.Buffer;
 import io.vertx.core.http.HttpServerOptions;
+import io.vertx.core.net.PemKeyCertOptions;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.handler.BodyHandler;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,7 +22,7 @@ import org.slf4j.LoggerFactory;
 public class TopicWebhook extends AbstractVerticle {
     private static final Logger log = LoggerFactory.getLogger(TopicWebhook.class.getName());
 
-    private static final int port = 8080;
+    private static final int port = 8443;
 
     public TopicWebhook(TopicWebhookConfig config) throws Exception {
         log.info("Creating Kafka Topic Initializer (KTI) controller");
@@ -39,7 +45,10 @@ public class TopicWebhook extends AbstractVerticle {
 
     private void startHttpServer(Handler<AsyncResult<Void>> resultHandler) {
         Router router = configureRouter();
+
         HttpServerOptions httpOptions = new HttpServerOptions();
+        setSsl(httpOptions);
+
         log.info("Starting web server on port {}", port);
         vertx.createHttpServer(httpOptions)
                 .requestHandler(router::accept)
@@ -53,20 +62,37 @@ public class TopicWebhook extends AbstractVerticle {
                         resultHandler.handle(Future.failedFuture(res.cause()));
                     }
                 });
+    }
 
+    private void setSsl(HttpServerOptions httpServerOptions) {
+        httpServerOptions.setSsl(true);
 
+        PemKeyCertOptions pemKeyCertOptions = new PemKeyCertOptions()
+                .setKeyValue(Buffer.buffer(new BufferedReader(new InputStreamReader(getClass().getResourceAsStream("/initializer-key.pem"))).lines().collect(Collectors.joining("\n"))))
+                .setCertValue(Buffer.buffer(new BufferedReader(new InputStreamReader(getClass().getResourceAsStream("/initializer.pem"))).lines().collect(Collectors.joining("\n"))));
+        httpServerOptions.setPemKeyCertOptions(pemKeyCertOptions);
     }
 
     private Router configureRouter() {
         Router router = Router.router(vertx);
         router.route().handler(BodyHandler.create());
-        router.route("/*").handler(this::admit);
+        router.route("/*").handler(this::handleRequest);
 
         return router;
     }
 
-    private void admit(RoutingContext routingContext) {
+    private void handleRequest(RoutingContext routingContext) {
         log.info("Received {} request on {} with body {}", routingContext.request().method().name(), routingContext.request().absoluteURI(), routingContext.getBodyAsString());
+
+        //new JSON().deserialize(routingContext.getBodyAsString(), V1alpha1Admission)
+
         routingContext.response().setStatusCode(HttpResponseStatus.NOT_IMPLEMENTED.code()).end();
+    }
+
+    private void admit() {
+        //log.info("Received {} request on {} with body {}", routingContext.request().method().name(), routingContext.request().absoluteURI(), routingContext.getBodyAsString());
+
+
+        //routingContext.response().setStatusCode(HttpResponseStatus.NOT_IMPLEMENTED.code()).end();
     }
 }
